6c599087be379132a7317bea9a0597a9c4441c52
Tanchinsan Vitalii <tanchinsan@gmail.com>
2024-08-03 22:44:57 +1000

    Добавьте файлы проекта.

---
+++ OtusHW.Delegates,Events/DirectoryScanner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace OtusHW.Delegates_Events
+{
+    public class FileFoundEventArgs : EventArgs //класс аргументов, т.е. того что будет передовать Event
+                                                //в Delegate (метод бработчик в делегате)
+                                                //только почему теперь каждый раз создаётся экземпляр класса? или нет?
+    {
+        public string FileName { get; }
+
+        public FileFoundEventArgs(string fileName)
+        {
+            FileName = fileName;
+        }
+    }
+
+    public class DirectoryScanner
+    {
+        public event EventHandler<FileFoundEventArgs> FileFound;
+
+        public void ScanDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                foreach (var fileName in Directory.GetFiles(path))
+                {
+                    OnFileFound(fileName);
+                }
+            }
+            else
+            {
+                throw new DirectoryNotFoundException($"The directory '{path}' does not exist.");
+            }
+        }
+
+        protected virtual void OnFileFound(string fileName)
+        {
+            FileFound?.Invoke(this, new FileFoundEventArgs(fileName));
+        }
+    }
+
+}
---
+++ OtusHW.Delegates,Events/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace OtusHW.Delegates_Events
+{
+    public delegate float MyDelegate(IEnumerable collection);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection<int> list = new Collection<int>() {1,2,3,4,5,6,7,8,9 }; 
+            var scanner = new DirectoryScanner();
+            scanner.FileFound += OnFileFound;
+            scanner.ScanDirectory("C:\\");
+            list.GetMax(element=> element.);
+        }
+
+        private static void OnFileFound(object sender, FileFoundEventArgs e)
+        {
+            Console.WriteLine($"File found: {e.FileName}");
+        }
+    }
+    public static class MyExtensions //hjn t,fk, тут мы должны знать что содержится в коллекции, ибо надо знать какой метод вызывать, не?
+                                     //(ленс или каунт, или наибольшее число)
+    {
+        public static T GetMax<T>(this IEnumerable collection, Func<T, float> convertToNumber) where T : class
+        {
+            if (collection == null)
+                throw new ArgumentException("Collection is null or empty");
+
+            T maxElement = null;
+
+            foreach ( var element in collection)
+            {
+                if (element > maxElement)
+                {
+                    maxElement = element;
+                }
+            }
+
+            return maxElement;
+        }
+    }
+}
